b8875e207f54b4079ace49691bc665485dfe0da3
prabuddhaxdev <prabuddha1749@gmail.com>
2025-08-25 01:36:50 +0530

    Add EmptyState boilerplate

--- app/dashboard/_components/EmptyState.jsx
+++ app/dashboard/_components/EmptyState.jsx
@@ -1,8 +1,14 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import React from 'react'
 
-const EmptyState = () => {
+function EmptyState (){
   return (
-    <div>EmptyState</div>
+    <div className='flex items-center justify-center mt-10 flex-col'>
+      <Image src={'./logo.svg'} width={200} height={200} alt='placeholder' />
+      <h2 className='font-medium text-lg text-gray-500'>Create new AI Interior Design for your room</h2>
+      <Button className='mt-5'>+ Redesign Room</Button>
+    </div>
   )
 }
 
